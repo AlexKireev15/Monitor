0cd5d5fe108ba06831083880e2c327ba4d590b67
Alex Kireev <alexkireev15@gmail.com>
2022-01-24 18:04:06 +0700

    Rework main - added MainLoop class

--- Monitor/src/main.cpp
+++ Monitor/src/main.cpp
@@ -9,49 +9,43 @@
 #include "GUI/CreateConnectionWindow.h"
 #include "DCSConnection/DCSConnection.h"
 
-int main(int argc, char ** argv)
+class MainLoop
 {
-	sf::RenderWindow window(sf::VideoMode(960, 600), "");
-	window.setVerticalSyncEnabled(true);
-	ImGui::SFML::Init(window);
-
-	sf::Color bgColor;
-	float color[3] = { 0.f, 0.f, 0.f };
-
-	std::string windowTitle = "Monitor";
-	window.setTitle(windowTitle);
-
-	ImGui::GetIO().WantCaptureKeyboard = true;
-
-	auto dcsConnection = std::make_shared<DCSConnection>();
-	std::list<std::shared_ptr<GUI::Element>> elements;
-	auto addElement = [&elements](const std::shared_ptr<GUI::Element>& element)
+public:
+	MainLoop(const std::string & windowTitle, unsigned width, unsigned height) :
+		m_window(sf::RenderWindow(sf::VideoMode(width, height), windowTitle))
 	{
-		if (element && std::find_if(elements.begin(), elements.end(), [&element](auto pElement) { return element->GetName() == pElement->GetName(); }) == elements.end())
-			elements.push_back(element);
-	};
-	elements.push_back(std::make_shared<GUI::TopMenuWindow>(dcsConnection, addElement));
-	elements.push_back(std::make_shared<GUI::CreateConnectionWindow>(dcsConnection, addElement));
+		m_window.setVerticalSyncEnabled(true);
+		ImGui::SFML::Init(m_window);
+		ImGui::GetIO().WantCaptureKeyboard = true;
 
-	sf::Clock deltaClock;
-	while (window.isOpen())
+		auto dcsConnection = std::make_shared<DCSConnection>();
+		auto bindAddElement = std::bind(&MainLoop::AddElement, this, std::placeholders::_1);
+		m_elements.push_back(std::make_shared<GUI::TopMenuWindow>(dcsConnection, bindAddElement));
+		m_elements.push_back(std::make_shared<GUI::CreateConnectionWindow>(dcsConnection, bindAddElement));
+	}
+
+	~MainLoop()
 	{
-		sf::Event event;
-		while (window.pollEvent(event))
+		ImGui::SFML::Shutdown();
+	}
+
+	void AddElement(const std::shared_ptr<GUI::Element>& element)
+	{
+		auto findByName = [&element](auto pElement)
 		{
-			ImGui::SFML::ProcessEvent(event);
+			return element->GetName() == pElement->GetName();
+		};
 
-			if (event.type == sf::Event::Closed)
-			{
-				window.close();
-			}
+		if (element && std::find_if(m_elements.begin(), m_elements.end(), findByName) == m_elements.end())
+		{
+			m_elements.push_back(element);
 		}
+	}
 
-		ImGui::GetIO().KeysData[ImGui::GetKeyIndex(ImGuiKey_O)].Down = sf::Keyboard::isKeyPressed(sf::Keyboard::O);
-
-		ImGui::SFML::Update(window, deltaClock.restart());
-
-		for (auto elementIt = elements.begin(); elementIt != elements.end();)
+	void EraseClosedElements()
+	{
+		for (auto elementIt = m_elements.begin(); elementIt != m_elements.end();)
 		{
 			auto element = *elementIt;
 			if (element->IsOpened())
@@ -61,15 +55,49 @@ int main(int argc, char ** argv)
 			}
 			else
 			{
-				elementIt = elements.erase(elementIt);
+				elementIt = m_elements.erase(elementIt);
 			}
 		}
-
-		window.clear(bgColor);
-		ImGui::SFML::Render(window);
-		window.display();
 	}
-    
-	ImGui::SFML::Shutdown();
+
+	void Run()
+	{
+		sf::Clock deltaClock;
+		while (m_window.isOpen())
+		{
+			sf::Event event;
+			while (m_window.pollEvent(event))
+			{
+				ImGui::SFML::ProcessEvent(event);
+
+				if (event.type == sf::Event::Closed)
+				{
+					m_window.close();
+				}
+			}
+
+			ImGui::GetIO().KeysData[ImGui::GetKeyIndex(ImGuiKey_O)].Down = sf::Keyboard::isKeyPressed(sf::Keyboard::O);
+
+			ImGui::SFML::Update(m_window, deltaClock.restart());
+			EraseClosedElements();
+
+			m_window.clear();
+			ImGui::SFML::Render(m_window);
+			m_window.display();
+		}
+	}
+
+
+private:
+	sf::RenderWindow m_window;
+
+	std::shared_ptr<DCSConnection> m_dcsConnection;
+	std::list<std::shared_ptr<GUI::Element>> m_elements;
+};
+
+int main(int argc, char ** argv)
+{
+	MainLoop loop("Monitor", 900u, 600u);
+	loop.Run();
 	return 0;
 }
